ae73cf791075e7ee5e5ca444268f867ee5c7fdcd
Ana Rita Pena <71759725+ppyarpe@users.noreply.github.com>
2022-04-12 17:03:32 +0100

    testing

--- Smooth_Random_Trees/testing_smooth_3v.py
+++ Smooth_Random_Trees/testing_smooth_3v.py
@@ -351,7 +351,7 @@ for total_budget in total_budgets_list:
 import matplotlib.pyplot as plt
 plt.plot(perf['Budget'],perf['Accuracy'],'--',label='Accuracy')
 plt.scatter(perf['Budget'],perf['Accuracy'],s=20)
-plt.axis([0,10,0.7,0.8])
+plt.axis([0,10,0.7,1])
 plt.xlabel("Privacy Loss")
 plt.legend()
 plt.show()
